eff6d9df7fec4ca3eda83359bbeaa962ee5cf98f
vijaya <vijayalakshmi.daya@gmail.com>
2025-01-30 16:47:32 +1100

    Installed all Dependencies till tailwind

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
+const App = () => {
+  return <div className="txt-3xl font-bold">From App</div>;
+};
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  purge: false,
+  darkMode: false,
+  theme: {
+    extend: {},
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
